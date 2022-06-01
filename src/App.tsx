6985909dc617e4423a5fdccd395abd9633b67c65
Raphael Mansueto <raphaelm@Raphaels-MacBook-Pro.local>
2022-06-01 13:14:47 +0800

    eslint integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mansueto's Test Project<br/>
-          Init workflow changes<br/>
-          Verify Secret Key is okay<br/>
-          Wrong Secret Key detected
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Mansueto&apos;s Test Project
+                    <br />
+                    Init workflow changes
+                    <br />
+                    Verify Secret Key is okay
+                    <br />
+                    Wrong Secret Key detected
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
